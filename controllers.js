47b4dc4e62ea55ae7f339cb4d38cbff7d3a30550
bures <richardbures@email.cz>
2016-07-11 16:51:45 +0200

    not compiled venice fix fix

--- controllers.js
+++ controllers.js
@@ -1,10 +1,10 @@
 'use strict';
-import BillingPlanController from '/Controllers/BillingPlanController.js';
-import BlogArticleController from '/Controllers/BlogArticleController.js';
-import ContentController from '/Controllers/ContentController.js';
-import ProductAccessController from '/Controllers/ProductAccessController.js';
-import ProductController from '/Controllers/ProductController.js';
-import UserController from '/Controllers/UserController.js';
+import BillingPlanController from './Controllers/BillingPlanController.js';
+import BlogArticleController from './Controllers/BlogArticleController.js';
+import ContentController from './Controllers/ContentController.js';
+import ProductAccessController from './Controllers/ProductAccessController.js';
+import ProductController from './Controllers/ProductController.js';
+import UserController from './Controllers/UserController.js';
 
 export default {
     'BillingPlanController': BillingPlanController,
